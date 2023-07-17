9e6326232b937ae6ffac20ce696ad34d416a71e7
Jeanpier Valera <jvalera@unsa.edu.pe>
2023-07-17 12:17:32 -0500

    agregar

--- project/Aplication1/models/Teacher.py
+++ project/Aplication1/models/Teacher.py
@@ -12,5 +12,7 @@ class Teacher(models.Model):
     organization_id =  models.ForeignKey(Organization, on_delete = models.CASCADE)
     user_id = models.ForeignKey(User, on_delete = models.CASCADE)
     teacher_created = models.DataTimeField(editable= False, null= False)
-    taecher_modified= models.DataTimeField(editable= True, null=False, auto_now=True)
+    teacher_modified= models.DataTimeField(editable= True, null=False, auto_now=True)
     teacher_name = models.CharField(max_length=100)
+    teacher_status = models.BooleanField()
+    teacher_email = models.EmailField()
