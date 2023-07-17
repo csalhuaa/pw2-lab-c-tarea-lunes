449a913b131acaeda5c9f0edf800f8a3fc192e85
Geraldine04Umasi <gumasi@unsa.edu.pe>
2023-07-17 12:03:17 -0500

    tabla student

---
+++ project/Aplication1/models/Student.py
@@ -0,0 +1,16 @@
+from django.db import models
+import uuid
+from . import *
+
+class Group(models.Model):
+    student_id = models.UUIDField(
+        primary_key = True,
+        default = uuid.uuid4,
+        editable = False
+    )
+    organization_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    school_id = models.ForeignKey(School, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    student_created = models.DateTimeField()
+    student_modified = models.DateTimeField()
+    student_status = models.BooleanField()
