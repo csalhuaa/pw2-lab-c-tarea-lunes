dc8cb0d26110b47c1ca365c17197e9882016f44e
Jeanpier Valera <jvalera@unsa.edu.pe>
2023-07-17 12:11:30 -0500

    modificaciones teacher

---
+++ project/Aplication1/models/Teacher.py
@@ -0,0 +1,16 @@
+from django.db import models
+import UUIDfied
+
+from . import *
+
+class Teacher(models.Model):
+    teacher_id = models.UUIDfied(
+        primary_key = True,
+        default = uuid.uuid4,
+        editable = False
+    )
+    organization_id =  models.ForeignKey(Organization, on_delete = models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete = models.CASCADE)
+    teacher_created = models.DataTimeField(editable= False, null= False)
+    taecher_modified= models.DataTimeField(editable= True, null=False, auto_now=True)
+    teacher_name = models.CharField(max_length=100)
