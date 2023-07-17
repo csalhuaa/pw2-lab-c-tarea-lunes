aab24cbb04672aff2ce654bcdd8928a03d08fb04
csalhuaa <121203340+csalhuaa@users.noreply.github.com>
2023-07-17 16:55:32 +0000

    creacionde moedlos Organization.py, School.py y Semester.py

---
+++ project/Aplication1/models/Organization.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Organization(models.Model):
+    organization_id = models.UUIDField(
+        primary_key = True,
+        default = uuid.uuid4,
+        editable = False)
+    organization_created = models.DateTimeField(editable=False, null=False, auto_now_add=True)
+    organization_modified = models.DateTimeField(null=False, auto_now=True)
+    organization_name = models.CharField(max_length=100)
+    organization_status =  models.BooleanField()
---
+++ project/Aplication1/models/School.py
@@ -0,0 +1,13 @@
+from django.db import models
+from .Organization import Organization
+
+class School(models.Model):
+    School_id = models.UUIDField(
+        primary_key = True,
+        default = uuid.uuid4,
+        editable = False)
+    organization_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    school_created = models.DateTimeField(editable=False, null=False, auto_now_add=True)
+    school_modified = models.DateTimeField(null=False, auto_now=True)
+    school_name = models.CharField(max_length=100)
+    school_status =  models.BooleanField()
