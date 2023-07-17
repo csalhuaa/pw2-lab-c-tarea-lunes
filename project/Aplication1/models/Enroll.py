08c670492dc83f33010868c7ce4992c141bf0dc0
Gabriel-Bernedo <132683380+Gabriel-Bernedo@users.noreply.github.com>
2023-07-17 11:48:40 -0500

    Create Enroll.py

---
+++ project/Aplication1/models/Enroll.py
@@ -0,0 +1,15 @@
+from django.db import models
+import uuid
+from . import *
+
+class Enroll(models.Model):
+    enroll_id = models.UUIDField(
+        primary_key = True,
+        default = uuid.uuid4,
+        editable = False)
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
+    organization_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    enroll_created = models.DateTimeField()
+    enroll_modified = models.DateTimeField()
+    enroll_status = models.BooleanField()
